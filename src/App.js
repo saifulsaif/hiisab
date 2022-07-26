2ff4433b8af7cf37dbcbb8968110b1361d646fd5
saifulsaif <saifulsaif5854@gmail.com>
2022-07-27 01:27:09 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home'
+import "./App.css";
+import Login from "./pages/auth/Login";
 
 function App() {
   return (
     <div className="App">
-     <Home />
+      <Login />
     </div>
   );
 }
