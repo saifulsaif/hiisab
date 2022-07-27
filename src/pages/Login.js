b40ebee413f9c23a1756c026c7a847bcbe9363a0
saifulsaif <saifulsaif5854@gmail.com>
2022-07-28 00:44:00 +0600

    setup admin component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,7 +5,6 @@ const Login = () => (
   <div>
     <div class="container position-sticky z-index-sticky top-0">
       <div class="row">
-        <div class="col-12">{/* <Header /> */}</div>
         <div class="col-12">
           <nav class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
             <div class="container-fluid pe-0">
@@ -33,13 +32,16 @@ const Login = () => (
               <div class="collapse navbar-collapse" id="navigation">
                 <ul class="navbar-nav mx-auto ms-xl-auto me-xl-7">
                   <li class="nav-item">
-                    <Link to="/home"> Dashboard </Link>
+                    <div class="nav-link me-2">
+                      <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
+                      <Link to="/home"> Dashboard </Link>
+                    </div>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link me-2" href="assets/pages/profile.html">
+                    <div class="nav-link me-2">
                       <i class="fa fa-user opacity-6 text-dark me-1"></i>
-                      Profile
-                    </a>
+                      <Link to="/home"> Home </Link>
+                    </div>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link me-2" href="assets/pages/sign-up.html">
