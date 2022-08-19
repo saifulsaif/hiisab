25ba45dfd17b74402db58d769b3d61f4a1d64edb
saifulsaif <saifulsaif5854@gmail.com>
2022-08-19 23:34:03 +0600

    renter component

--- src/component/property/FloorList.js
+++ src/component/property/FloorList.js
@@ -1,43 +1,40 @@
 import React from "react";
 
 const FloorList = (props) => {
-  const floors = props.floors.map((element) => {
-    return (
-      <li>
-        <div class="card">
-          <div class="card-body p-3">
-            <div class="row">
-              <div class="col-8">
-                <div class="numbers">
-                  <p class="text-sm mb-0 text-capitalize font-weight-bold">
-                    {element.name}
-                  </p>
-                  <h5 class="font-weight-bolder mb-0">
-                    <div class="progress w-${element.percentage}">
-                      <div
-                        class="progress-bar bg-dark w-90"
-                        role="progressbar"
-                        aria-valuenow="90"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                      ></div>
-                    </div>
-                  </h5>
-                </div>
+ 
+  return (
+    <li>
+      <div class="card">
+        <div class="card-body p-3">
+          <div class="row">
+            <div class="col-8">
+              <div class="numbers">
+                <p class="text-sm text-capitalize font-weight-bold">
+                  {props.floors.name}
+                </p>
+                <h5 class="font-weight-bolder">
+                  <div class="progress w-75">
+                    <div
+                      className={`progress-bar bg-dark w-${props.floors.percentage}`}
+                      role="progressbar"
+                      aria-valuenow="90"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </h5>
               </div>
-              <div class="col-4 text-end">
-                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md floor-text">
-                  {element.number}
-                </div>
+            </div>
+            <div class="col-4 text-end">
+              <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md floor-text">
+                {props.floors.number}
               </div>
             </div>
           </div>
         </div>
-      </li>
-    );
-  });
-
-  return <div>{floors}</div>;
+      </div>
+    </li>
+  );
 };
 
 export default FloorList;
--- src/pages/Floor.js
+++ src/pages/Floor.js
@@ -27,6 +27,16 @@ const Floor = () => {
       name: "Denmark",
       percentage: 25,
     },
+    {
+      number: 5,
+      name: "England",
+      percentage: 90,
+    },
+    {
+      number: 6,
+      name: "Franch",
+      percentage: 40,
+    },
   ];
 
   var renters = [
@@ -74,10 +84,44 @@ const Floor = () => {
         verified: true,
       },
     },
+    {
+      name: "Justin",
+      profile:
+        "https://images.pexels.com/photos/10016478/pexels-photo-10016478.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      info: {
+        advance: 40000,
+        rent: 2600,
+        document: true,
+        verified: true,
+      },
+    },
+    {
+      name: "Ziyon malek",
+      profile:
+        "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      info: {
+        advance: 80000,
+        rent: 7600,
+        document: false,
+        verified: true,
+      },
+    },
   ];
 
-  const [units, setUnites] = useState(renters);
-  const [floors, setFloot] = useState(floors_data);
+  const [units] = useState(renters);
+  const [floors] = useState(floors_data);
+
+  const unitList = units.map((element) => {
+    return (
+      <div class="col-12 col-md-6">
+        <Unit units={element} />
+      </div>
+    );
+  });
+
+  const floorView = floors.map((element) => {
+    return <FloorList floors={element} />;
+  });
 
   return (
     <div>
@@ -87,11 +131,75 @@ const Floor = () => {
         <div class="row">
           <div class="col-12 col-md-3">
             <ul>
-              <FloorList floors={floors} />
+              <li>
+                <div class="card">
+                  <div class="card-body add-floor-card">
+                    <div class="row">
+                      <div class="col-8">
+                        <div class="numbers">
+                          <h5> Level / Floor</h5>
+                        </div>
+                      </div>
+                      <div class="col-4 text-end">
+                        <button className="btn bg-gradient-primary w-100">
+                          Add
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              {floorView}
             </ul>
           </div>
           <div class="col-12 col-md-9">
-            <Unit units={units} />
+            <div className="row">
+              {unitList}
+              <div class="col-12 col-md-6">
+                <div class="card">
+                  <div class="card-header pb-0 p-3">
+                    <div class="d-flex align-items-center">
+                      <h6 class="mb-0">Renter Name </h6>
+                      <button
+                        type="button"
+                        class="btn btn-icon-only btn-rounded btn-outline-secondary mb-0 ms-2 btn-sm d-flex align-items-center justify-content-center ms-auto"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="bottom"
+                      >
+                        <i class="fas fa-info" aria-hidden="true"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body p-3">
+                    <div class="row">
+                      <div class="col-5 text-center">
+                        <div>
+                          <input
+                            class="border-profile"
+                            type="image"
+                            alt="photo"
+                            src="https://images.pexels.com/photos/10016478/pexels-photo-10016478.jpeg?auto=compress&amp;cs=tinysrgb&amp;w=1260&amp;h=750&amp;dpr=1"
+                          />
+                        </div>
+                      </div>
+                      <div class="col-7">
+                        <div class="card h-100">
+                          <div class="card-body d-flex flex-column justify-content-center text-center">
+                            <a href={""}>
+                              <i
+                                class="fa fa-plus text-secondary mb-3"
+                                aria-hidden="true"
+                              ></i>
+                              <h5 class="text-secondary"> New Renter </h5>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
